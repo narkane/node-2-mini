11593982db5d7c13193f40090c10a9d494e81c0a
Hans Thummel <enikath@gmail.com>
2018-12-13 14:46:09 -0600

    working

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const massive = require("massive");
+
+const controller = require("./controller");
+
+require("dotenv").config();
 
 const app = express();
-app.use( bodyParser.json() );
-app.use( cors() );
+const port = process.env.PORT;
 
-const port = process.env.PORT || 3000
-app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+  app.set("db", dbInstance);
 
+  //   dbInstance
+  //     .new_planes()
+  //     .then(planes => console.log(planes))
+  //     .catch(err => console.log(err));
+
+  //   dbInstance
+  //     .get_planes()
+  //     .then(planes => {
+  //       return console.log(planes);
+  //     })
+  //     .catch(err => {
+  //       return console.log(err);
+  //     });
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/api/planes", controller.get_planes);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
